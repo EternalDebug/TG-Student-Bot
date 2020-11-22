86018889c92b5b92901f228333a14b941e6cfba5
EternalDebug <71968876+EternalDebug@users.noreply.github.com>
2020-11-22 18:18:17 +0300

    Add files via upload

---
+++ Project_WELT/Project_WELT/Welt_Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Telegram.Bot;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static Project_WELT.Welt_Secondary;
+
+
+namespace Project_WELT
+{
+
+    class Welt_Main
+    {
+        static readonly TelegramBotClient Welt = new TelegramBotClient("1490665480:AAEF9wmeDVeFx1hvs3kiOZ_4mxn82bePmQo");
+
+        static void Main(string[] args)
+        {
+            Welt.OnMessage += Welt_OnMessage;
+            Welt.StartReceiving();
+            Console.ReadLine();
+            Welt.StopReceiving();
+        }
+
+        static void Welt_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type ==Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                string m = e.Message.Text;
+                Console.WriteLine(m);
+                if (m == "Du")
+                {
+                    Welt.SendTextMessageAsync(e.Message.Chat.Id, "Du hast");
+                }
+                if (m == "Du hast mich")
+                {
+                    Welt.SendTextMessageAsync(e.Message.Chat.Id, "Du hast mich gefragt");
+                }
+                if (m == "Шути")
+                {
+                    SendRandMilJoke(sender, e);
+                }
+            }
+        }
+    }
+}
---
+++ Project_WELT/Project_WELT/Welt_Secondary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static Project_WELT.Welt_Main;
+using Telegram.Bot;
+
+namespace Project_WELT
+{
+    class Welt_Secondary
+    {
+        static readonly TelegramBotClient Welt = new TelegramBotClient("1490665480:AAEF9wmeDVeFx1hvs3kiOZ_4mxn82bePmQo");
+        public static void SendRandMilJoke(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string path = "MilJoke.txt";
+            string[] joke = File.ReadAllLines(path).ToArray();
+            Random rnd = new Random();
+
+            Welt.SendTextMessageAsync(e.Message.Chat.Id, joke[rnd.Next(0,joke.Length)]);
+           
+            
+        }
+    }
+}
